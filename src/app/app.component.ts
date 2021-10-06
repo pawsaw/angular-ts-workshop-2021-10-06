43b8864add943e2ca7da8f2c0a30386397e6a561
Pawel Sawicki <public@psawicki.de>
2021-10-06 16:34:48 +0200

    Adding an input to the book-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bookm';
+
+  book = {
+    title: 'How to win friends',
+    author: 'Dale Carnegie',
+    abstract: 'In this book ...',
+  };
 }
